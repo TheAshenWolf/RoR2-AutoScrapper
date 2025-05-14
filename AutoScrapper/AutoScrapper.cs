18f019609fe4a709b660237cbb4a9e0e678b3b3b
TheAshenWolf <tawdevelopment@gmail.com>
2025-05-14 13:38:51 +0200

    Version bump
    0.2.1 -> 0.3.0
    Some of this has been done already, but merge reverted this change. Updated changelog to include profiles.

--- AutoScrapper/AutoScrapper.cs
+++ AutoScrapper/AutoScrapper.cs
@@ -24,7 +24,7 @@ namespace AutoScrapper
         public const string PLUGIN_GUID = PLUGIN_AUTHOR + "." + PLUGIN_NAME;
         public const string PLUGIN_AUTHOR = "TheAshenWolf";
         public const string PLUGIN_NAME = "AutoScrapper";
-        public const string PLUGIN_VERSION = "0.2.1";
+        public const string PLUGIN_VERSION = "0.3.0";
 
         /// Link to the plugin's config file
         public AutoScrapperConfig config;
